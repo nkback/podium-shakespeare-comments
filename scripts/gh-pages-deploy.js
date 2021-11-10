5e5eb9b6dd32a2d8ce82c7cde4f3333beab4faac
Nate Back <nkback@gmail.com>
2021-11-09 22:20:28 -0700

    Fix pathing issue

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -8,12 +8,12 @@ const fs = require("fs");
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    const folderName = fs.existsSync("dist") ? ".\\dist" : ".\\build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rd", ["-r", folderName]);
+    await execa("RMDIR", ["/S", "/Q", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
